36daaf29ac10fd7afad92e43249ada9cfc32fd15
Jakob Østergaard Jensen <joj@netic.dk>
2024-08-22 10:34:33 +0200

    cmd/webhook/provider.go: Fix wrong handling of TXT record values
    Earlier there was a problem with extra double quotes seemingly being added by
    external-dns and Tidy not accepting it. The workaround being to remove any
    outer double quotes on TXT records and re-adding them when serving them back to
    external-dns, though this breaks external-dns.
    
    The solution it seems is that the double quotes are removed when creating the
    record and nothing else. External-dns seems no to complain when these values
    are read back even though the value has technically changed a bit.
    
    Maybe I'm overlooking something but this seems to work.

--- cmd/webhook/provider.go
+++ cmd/webhook/provider.go
@@ -188,18 +188,11 @@ func (p *tidyProvider) parseTidyRecord(record *tidyRecord) *Endpoint {
 	// Convert TTL to TTL type
 	ttl := endpoint.TTL(record.TTL)
 
-	// TXT values from External-DNS are wrapped in "" which Tidy DNS doesn't
-	// support. So when converting back they are re-wrapped.
-	target := record.Destination
-	if record.Type == "TXT" {
-		target = "\"" + target + "\""
-	}
-
 	// Convert description into the ProviderSpec
 	providerSpec := p.descriptionToProviderSpec(record.Description)
 
 	// Create Endpoint
-	endpoint := endpoint.NewEndpointWithTTL(dnsName, record.Type, ttl, target)
+	endpoint := endpoint.NewEndpointWithTTL(dnsName, record.Type, ttl, record.Destination)
 	endpoint.ProviderSpecific = providerSpec
 
 	return endpoint
@@ -242,8 +235,6 @@ func (p *tidyProvider) deleteEndpoint(allRecords []tidyRecord, endpoint *Endpoin
 func (p *tidyProvider) findRecords(records []tidyRecord, endpoint *Endpoint) []tidyRecord {
 	found := []tidydns.Record{}
 	for _, target := range endpoint.Targets {
-		target = strings.Trim(target, "\"")
-
 		for _, record := range records {
 			dnsName := ""
 			if record.Name == "." {
@@ -275,6 +266,10 @@ func (p *tidyProvider) createRecord(zones []tidydns.Zone, endpoint *Endpoint) {
 	description := p.providerSpecToDescription(endpoint.ProviderSpecific)
 
 	for _, target := range endpoint.Targets {
+		// For some reason external-dns wraps the value of certain TXT records
+		// with extra double quotes. This isn't supported by Tidy and it will
+		// refuse to save and removing them seemingly causes no issues for
+		// external-dns when read back.
 		target = strings.Trim(target, "\"")
 
 		newRec := &tidyRecord{
